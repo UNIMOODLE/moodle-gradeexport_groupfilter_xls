ae0a0be43a4c86d07bff7da8f007c642c8c21981
joan-carbassa-ithinkupc <131751652+joan-carbassa-ithinkupc@users.noreply.github.com>
2023-12-27 11:02:33 +0100

    Add files via upload

---
+++ lang/gl/gradeexport_groupfilter_xls.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+// Project implemented by the &quot;Recovery, Transformation and Resilience Plan.
+// Funded by the European Union - Next GenerationEU&quot;.
+//
+// Produced by the UNIMOODLE University Group: Universities of
+// Valladolid, Complutense de Madrid, UPV/EHU, León, Salamanca,
+// Illes Balears, Valencia, Rey Juan Carlos, La Laguna, Zaragoza, Málaga,
+// Córdoba, Extremadura, Vigo, Las Palmas de Gran Canaria y Burgos.
+/**
+ * Display information about all the gradeexport_groupfilter_xls modules in the requested course. *
+ * @package groupfilter_xls
+ * @copyright 2023 Proyecto UNIMOODLE
+ * @author UNIMOODLE Group (Coordinator) &lt;direccion.area.estrategia.digital@uva.es&gt;
+ * @author Miguel Gutiérrez (UPCnet) &lt;miguel.gutierrez.jariod@upcnet.es&gt;
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Strings for component 'gradeexport_groupfilter_xls', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   gradeexport_xls
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['eventgradeexported'] = 'Groupfilter XLS grao exportado';
+$string['pluginname'] = 'Groupfilter Folla de cálculo de Excel';
+$string['privacy:metadata'] = 'O complemento de exportación da folla de cálculo de Excel non almacena ningún dato persoal.';
+$string['timeexported'] = 'Última descarga deste curso';
+$string['groupfilter_xls:publish'] = 'Publicar a exportación de grao XLS de Groupfilter';
+$string['groupfilter_xls:view'] = 'Use a exportación de notas de Excel';
+
+// Added for grade_export_form.
+$string['userfieldsheader'] = 'Campos de usuario';
+$string['userfields_form'] = 'Selección de campos de usuario';
+$string['group'] = 'Grupos';
